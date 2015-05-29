bd0756042a15560be0473fd711b2e0950dba5d9e
Jacob Wright <jacwright@gmail.com>
2015-05-29 00:24:12 -0600

    Removing gulp dependency, using npm.

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.observer = require('./src/observer');
+exports.diff = require('./src/diff');
+exports.template = require('./src/template');
+exports.expression = require('./src/expression');
+exports.binding = require('./src/binding');
+exports.filter = require('./src/filter');
+exports.bindings = require('./src/bindings');
+exports.filters = require('./src/filters');
