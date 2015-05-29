f3f87dc918213ad75707ef597b719939643cbde2
Jacob Wright <jacwright@gmail.com>
2015-05-28 23:36:39 -0600

    Creates a flexible template/view implementation.

---
+++ src/util/toFragment.js
@@ -0,0 +1,102 @@
+module.exports = toFragment;
+
+// Convert stuff into document fragments. Stuff can be:
+// * A string of HTML text
+// * An element or text node
+// * A NodeList or HTMLCollection (e.g. `element.childNodes` or `element.children`)
+// * A jQuery object
+// * A script element with a `type` attribute of `"text/*"` (e.g. `<script type="text/html">My template code!</script>`)
+// * A template element (e.g. `<template>My template code!</template>`)
+function toFragment(html) {
+  var fragment;
+  if (html instanceof DocumentFragment) {
+    return html;
+  } else if (typeof html === 'string') {
+    return stringToFragment(html);
+  } else if (html instanceof Node) {
+    return nodeToFragment(html);
+  } else if (html.hasOwnProperty('length')) {
+    return listToFragment(html);
+  } else {
+    throw new TypeError('Unsupported Template Type: Cannot convert `' + html + '` into a document fragment.');
+  }
+}
+
+// Converts an HTML node into a document fragment. If it is a <template> node its contents will be used. If it is a
+// <script> node its string-based contents will be converted to HTML first, then used. Otherwise a clone of the node
+// itself will be used.
+function nodeToFragment(node) {
+  if (node.content instanceof DocumentFragment) {
+    return node.content;
+  } else if (node.tagName === 'SCRIPT') {
+    return stringToFragment(node.innerHTML);
+  } else {
+    var fragment = document.createDocumentFragment();
+    if (node.tagName === 'TEMPLATE') {
+      for (var i = 0, l = node.childNodes.length; i < l; i++) {
+        fragment.appendChild(node.childNodes[i]);
+      }
+    } else {
+      fragment.appendChild(node);
+    }
+    return fragment;
+  }
+}
+
+// Converts an HTMLCollection, NodeList, jQuery object, or array into a document fragment.
+function listToFragment(list) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0, l = list.length; i < l; i++) {
+    // Use toFragment since this may be an array of text, a jQuery object of `<template>`s, etc.
+    fragment.appendChild(toFragment(list[i]));
+  }
+  return fragment;
+}
+
+// Converts a string of HTML text into a document fragment.
+function stringToFragment(string) {
+  var templateElement;
+  templateElement = document.createElement('template');
+  templateElement.innerHTML = string;
+  return templateElement.content;
+}
+
+// If HTML Templates are not available (e.g. in IE) then use an older method to work with certain elements.
+if (!document.createElement('template').content instanceof DocumentFragment) {
+  stringToFragment = (function() {
+    var tagExp = /<([\w:-]+)/;
+
+    // Copied from jQuery (https://github.com/jquery/jquery/blob/master/LICENSE.txt)
+    var wrapMap = {
+      option: [ 1, '<select multiple="multiple">', '</select>' ],
+      legend: [ 1, '<fieldset>', '</fieldset>' ],
+      thead: [ 1, '<table>', '</table>' ],
+      tr: [ 2, '<table><tbody>', '</tbody></table>' ],
+      td: [ 3, '<table><tbody><tr>', '</tr></tbody></table>' ],
+      col: [ 2, '<table><tbody></tbody><colgroup>', '</colgroup></table>' ],
+      area: [ 1, '<map>', '</map>' ],
+      _default: [ 0, '', '' ]
+    };
+    wrapMap.optgroup = wrapMap.option;
+    wrapMap.tbody = wrapMap.tfoot = wrapMap.colgroup = wrapMap.caption = wrapMap.thead;
+    wrapMap.th = wrapMap.td;
+
+    return function stringToFragment(string) {
+      var tag = string.match(tagExp);
+      var parts = wrapMap[tag] || wrapMap._default;
+      var depth = parts[0];
+      var prefix = parts[1];
+      var postfix = parts[2];
+      var div = document.createElement('div');
+      div.innerHTML = prefix + string + postfix;
+      while (depth--) {
+        div = div.lastChild;
+      }
+      var fragment = document.createDocumentFragment();
+      while (div.firstChild) {
+        fragment.appendChild(div.firstChild);
+      }
+      return fragment;
+    };
+  })();
+}
