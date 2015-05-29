a68c04563998e1afb40d7b36fb3b892e028c284d
Jacob Wright <jacob.wright@riotgames.com>
2015-05-28 23:37:42 -0600

    Updates build to use browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,28 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var concat = require('gulp-concat');
-var coffee = require('gulp-coffee');
 var uglify = require('gulp-uglify');
-var docco = require("gulp-docco");
-var rename = require("gulp-rename");
-
-var srcFiles = [
-  'src/observer.coffee',
-  'src/controller.coffee',
-  'src/expression.coffee',
-  'src/binding.coffee',
-  'src/filter.coffee',
-  'src/bindings.coffee',
-  'src/filters.coffee',
-  'src/diff.coffee'
-];
-
-
+var docco = require('gulp-docco');
+var rename = require('gulp-rename');
+var browserify = require('browserify');
+var coffeeify = require('coffeeify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 
 gulp.task('default', [ 'dist', 'docs' ]);
 
 
 gulp.task('dist', function() {
-  return gulp.src(srcFiles)
-    .pipe(concat('chip-binding.js'))
-    .pipe(coffee({ bare: true })).on('error', gutil.log)
+  var b = browserify({
+    entries: ['src/template'],
+    extensions: ['.coffee'],
+    transforms: [coffeeify],
+    standalone: 'template'
+  });
+
+  return b.bundle()
+    .on('error', gutil.log)
+    .pipe(source('chip-binding.js'))
+    .pipe(buffer())
     .pipe(gulp.dest('dist'))
     .pipe(rename('chip-binding.min.js'))
     .pipe(uglify()).on('error', gutil.log)
@@ -35,12 +32,12 @@ gulp.task('dist', function() {
 
 
 gulp.task('docs', function() {
-  return gulp.src(srcFiles)
+  return gulp.src('src/*')
     .pipe(docco())
     .pipe(gulp.dest('docs'))
 });
 
 
-gulp.task('watch', [ 'default' ], function() {
-  return gulp.watch('src/*.coffee', [ 'default' ]);
+gulp.task('watch', [ 'dist' ], function() {
+  gulp.watch('src/**/*', [ 'default' ]);
 });
