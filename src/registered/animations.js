7fdc59f134e407c7d72189c6529c59cb6d6331da
Jacob Wright <jacwright@gmail.com>
2015-08-11 16:54:46 -0600

    Adds horizontal slide animation

--- src/registered/animations.js
+++ src/registered/animations.js
@@ -28,144 +28,165 @@ function registerDefaults(fragments) {
     }
   });
 
-
-  /**
-   * Slide down and up
-   */
-  fragments.registerAnimation('slide', {
-    options: {
-      duration: 300,
-      easing: 'ease-in-out'
-    },
-    animateIn: function(element, done) {
-      var height = element.getComputedCSS('height');
-      if (!height || height === '0px') {
-        return done();
-      }
-
-      element.style.overflow = 'hidden';
-      element.animate([
-        { height: '0px' },
-        { height: height }
-      ], this.options).onfinish = function() {
-        element.style.overflow = '';
-        done();
-      };
-    },
-    animateOut: function(element, done) {
-      var height = element.getComputedCSS('height');
-      if (!height || height === '0px') {
-        return done();
-      }
-
-      element.style.overflow = 'hidden';
-      element.animate([
-        { height: height },
-        { height: '0px' }
-      ], this.options).onfinish = function() {
-        element.style.overflow = '';
-        done();
-      };
-    }
-  });
-
+  var slides = {
+    slide: 'height',
+    slidev: 'height',
+    slideh: 'width'
+  };
 
   var animating = new Map();
 
+  function obj(key, value) {
+    var obj = {};
+    obj[key] = value;
+    return obj;
+  }
+
   /**
-   * Move items up and down in a list, slide down and up
+   * Slide down and up, left and right
    */
-  fragments.registerAnimation('slide-move', {
-    options: {
-      duration: 300,
-      easing: 'ease-in-out'
-    },
+  Object.keys(slides).forEach(function(name) {
+    var property = slides[name];
 
-    animateIn: function(element, done) {
-      var height = element.getComputedCSS('height');
-      if (!height || height === '0px') {
-        return done();
-      }
-
-      var item = element.view && element.view._repeatItem_;
-      if (item) {
-        animating.set(item, element);
-        setTimeout(function() {
-          animating.delete(item);
-        });
-      }
-
-      // Do the slide
-      element.style.overflow = 'hidden';
-      element.animate([
-        { height: '0px' },
-        { height: height }
-      ], this.options).onfinish = function() {
-        element.style.overflow = '';
-        done();
-      };
-    },
-
-    animateOut: function(element, done) {
-      var height = element.getComputedCSS('height');
-      if (!height || height === '0px') {
-        return done();
-      }
-
-      var item = element.view && element.view._repeatItem_;
-      if (item) {
-        var newElement = animating.get(item);
-        if (newElement && newElement.parentNode === element.parentNode) {
-          // This item is being removed in one place and added into another. Make it look like its moving by making both
-          // elements not visible and having a clone move above the items to the new location.
-          element = this.animateMove(element, newElement);
+    fragments.registerAnimation(name, {
+      options: {
+        duration: 300,
+        easing: 'ease-in-out'
+      },
+      animateIn: function(element, done) {
+        var value = element.getComputedCSS(property);
+        if (!value || value === '0px') {
+          return done();
         }
-      }
 
-      // Do the slide
-      element.style.overflow = 'hidden';
-      element.animate([
-        { height: height },
-        { height: '0px' }
-      ], this.options).onfinish = function() {
-        element.style.overflow = '';
-        done();
-      };
-    },
-
-    animateMove: function(oldElement, newElement) {
-      var placeholderElement;
-      var parent = newElement.parentNode;
-      if (!parent.__slideMoveHandled) {
-        parent.__slideMoveHandled = true;
-        if (window.getComputedStyle(parent).position === 'static') {
-          parent.style.position = 'relative';
+        element.style.overflow = 'hidden';
+        element.animate([
+          obj(property, '0px'),
+          obj(property, value)
+        ], this.options).onfinish = function() {
+          element.style.overflow = '';
+          done();
+        };
+      },
+      animateOut: function(element, done) {
+        var value = element.getComputedCSS(property);
+        if (!value || value === '0px') {
+          return done();
         }
+
+        element.style.overflow = 'hidden';
+        element.animate([
+          obj(property, value),
+          obj(property, '0px')
+        ], this.options).onfinish = function() {
+          element.style.overflow = '';
+          done();
+        };
       }
+    });
 
-      var origStyle = oldElement.getAttribute('style');
-      var style = window.getComputedStyle(oldElement);
-      placeholderElement = fragments.makeElementAnimatable(document.createElement(oldElement.nodeName));
-      placeholderElement.style.width = oldElement.style.width = style.width;
-      placeholderElement.style.height = oldElement.style.height = style.height;
-      placeholderElement.style.visibility = 'hidden';
 
-      oldElement.style.position = 'absolute';
-      oldElement.style.zIndex = 1000;
-      parent.insertBefore(placeholderElement, oldElement);
-      newElement.style.visibility = 'hidden';
+    /**
+     * Move items up and down in a list, slide down and up
+     */
+    fragments.registerAnimation(name + '-move', {
+      options: {
+        duration: 300,
+        easing: 'ease-in-out'
+      },
 
-      oldElement.animate([
-        { top: placeholderElement.offsetTop + 'px' },
-        { top: newElement.offsetTop + 'px' }
-      ], this.options).onfinish = function() {
-        placeholderElement.remove();
-        origStyle ? oldElement.setAttribute('style', origStyle) : oldElement.removeAttribute('style');
-        newElement.style.visibility = '';
-      };
+      animateIn: function(element, done) {
+        var value = element.getComputedCSS(property);
+        if (!value || value === '0px') {
+          return done();
+        }
 
-      return placeholderElement;
-    }
+        var item = element.view && element.view._repeatItem_;
+        if (item) {
+          animating.set(item, element);
+          setTimeout(function() {
+            animating.delete(item);
+          });
+        }
+
+        // Do the slide
+        element.style.overflow = 'hidden';
+        element.animate([
+          obj(property, '0px'),
+          obj(property, value)
+        ], this.options).onfinish = function() {
+          element.style.overflow = '';
+          done();
+        };
+      },
+
+      animateOut: function(element, done) {
+        var value = element.getComputedCSS(property);
+        if (!value || value === '0px') {
+          return done();
+        }
+
+        var item = element.view && element.view._repeatItem_;
+        if (item) {
+          var newElement = animating.get(item);
+          if (newElement && newElement.parentNode === element.parentNode) {
+            // This item is being removed in one place and added into another. Make it look like its moving by making both
+            // elements not visible and having a clone move above the items to the new location.
+            element = this.animateMove(element, newElement);
+          }
+        }
+
+        // Do the slide
+        element.style.overflow = 'hidden';
+        element.animate([
+          obj(property, value),
+          obj(property, '0px')
+        ], this.options).onfinish = function() {
+          element.style.overflow = '';
+          done();
+        };
+      },
+
+      animateMove: function(oldElement, newElement) {
+        var placeholderElement;
+        var parent = newElement.parentNode;
+        if (!parent.__slideMoveHandled) {
+          parent.__slideMoveHandled = true;
+          if (window.getComputedStyle(parent).position === 'static') {
+            parent.style.position = 'relative';
+          }
+        }
+
+        var origStyle = oldElement.getAttribute('style');
+        var style = window.getComputedStyle(oldElement);
+        var marginOffsetLeft = -parseInt(style.marginLeft);
+        var marginOffsetTop = -parseInt(style.marginTop);
+        var oldLeft = oldElement.offsetLeft;
+        var oldTop = oldElement.offsetTop;
+
+        placeholderElement = fragments.makeElementAnimatable(oldElement.cloneNode(true));
+        placeholderElement.style.width = oldElement.style.width = style.width;
+        placeholderElement.style.height = oldElement.style.height = style.height;
+        placeholderElement.style.opacity = '0';
+
+        oldElement.style.position = 'absolute';
+        oldElement.style.zIndex = 1000;
+        parent.insertBefore(placeholderElement, oldElement);
+        newElement.style.opacity = '0';
+
+        oldElement.animate([
+          { top: oldTop + marginOffsetTop + 'px', left: oldLeft + marginOffsetLeft + 'px' },
+          { top: newElement.offsetTop + marginOffsetTop + 'px', left: newElement.offsetLeft + marginOffsetLeft + 'px' }
+        ], this.options).onfinish = function() {
+          placeholderElement.remove();
+          origStyle ? oldElement.setAttribute('style', origStyle) : oldElement.removeAttribute('style');
+          newElement.style.opacity = '';
+        };
+
+        return placeholderElement;
+      }
+    });
+
   });
 
 }
