2c35b18e2672852b18d1e906bfaf6453d1724336
Jacob Wright <jacwright@gmail.com>
2015-09-12 12:31:56 -0600

    Adds "show" attribute.

--- src/registered/binders.js
+++ src/registered/binders.js
@@ -497,6 +497,63 @@ function registerDefaults(fragments) {
   fragments.registerAttribute('checked?', fragments.getAttributeBinder('value'));
 
 
+  /**
+   * Shows/hides an element conditionally. `if` should be used in most cases as it removes the element completely and is
+   * more effecient since bindings within the `if` are not active while it is hidden. Use `show` for when the element
+   * must remain in-DOM or bindings within it must continue to be processed while it is hidden. You should default to
+   * using `if`.
+   */
+  fragments.registerAttribute('show', {
+    animated: true,
+    updated: function(value) {
+      // For performance provide an alternate code path for animation
+      if (this.animate && this.context) {
+        this.updatedAnimated(value);
+      } else {
+        this.updatedRegular(value);
+      }
+    },
+
+    updatedRegular: function(value) {
+      if (value) {
+        this.element.style.display = '';
+      } else {
+        this.element.style.display = 'none';
+      }
+    },
+
+    updatedAnimated: function(value) {
+      this.lastValue = value;
+      if (this.animating) {
+        return;
+      }
+
+      this.animating = true;
+      function onFinish() {
+        this.animating = false;
+        if (this.lastValue !== value) {
+          this.updatedAnimated(this.lastValue);
+        }
+      }
+
+      if (value) {
+        this.element.style.display = '';
+        this.animateIn(this.element, onFinish);
+      } else {
+        this.animateOut(this.element, function() {
+          this.element.style.display = 'none';
+          onFinish.call(this);
+        });
+      }
+    },
+
+    unbound: function() {
+      this.element.style.display = '';
+      this.lastValue = null;
+      this.animating = false;
+    }
+  });
+
 
   /**
    * ## if, unless, else-if, else-unless, else
